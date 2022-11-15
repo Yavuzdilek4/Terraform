b3b8f65770abda84a47dcc27982b65f57617b6b6
Yavuzdilek4 <111000414+Yavuzdilek4@users.noreply.github.com>
2022-11-14 19:55:21 -0500

    updating terraform code:

---
+++ ec2.tf
@@ -0,0 +1,18 @@
+Provider "aws" {
+region     = "us-east-1"
+access_key ="__ACCESSKEY__"
+secret_key ="__SECRETKEY__"
+}
+
+resource "aws_instance" "ec2instance" {
+ami           = "ami-05fa00d4c63e32376"
+instance_type ="t2.micro"
+subnet_id = "subnet-01789892a7cc20dc0"
+key_name  = "KeYavuz"
+tags = {
+Name = "SERVER"
+Environment ="DEV"
+OS = "Linux"
+Managed = "IAC"
+}
+}
